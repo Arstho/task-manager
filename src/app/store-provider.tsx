2e264831e678d9174a28367656c9442e78db8b36
Aslan <119816056+Arstho@users.noreply.github.com>
2025-11-09 21:44:49 +0300

    Update store-provider.tsx

--- src/app/store-provider.tsx
+++ src/app/store-provider.tsx
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux'
 import { AppStore, makeStore } from '@/store/index'
 
 export default function StoreProvider({	children}: { children: React.ReactNode }) {
-	const storeRef = useRef<AppStore>()
+	const storeRef = useRef<AppStore | null>(null)
 
 	if (!storeRef.current) {
 		storeRef.current = makeStore()
